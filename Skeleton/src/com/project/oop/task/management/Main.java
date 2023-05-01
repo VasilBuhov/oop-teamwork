79238894d89c710329eeb07212b1755545cb87ab
Nikol Stoyneva <nikol.stoyneva@abv.bg>
2023-05-01 09:20:05 +0300

    Create an object from Engine class in main

--- Skeleton/src/com/project/oop/task/management/Main.java
+++ Skeleton/src/com/project/oop/task/management/Main.java
@@ -1,7 +1,13 @@
 package com.project.oop.task.management;
 
+import com.project.oop.task.management.core.TaskManagementEngineImpl;
+
 public class Main {
     public static void main(String[] args) {
+
+        TaskManagementEngineImpl engine = new TaskManagementEngineImpl();
+        engine.start();
+
 
     }
 }

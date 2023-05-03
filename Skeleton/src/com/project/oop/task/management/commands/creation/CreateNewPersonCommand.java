03c666b109f2e0377cea7969f66c094922b66190
Tatyana Velkova <tatyana.velkova@gmail.com>
2023-05-03 15:01:58 +0300

    Rename CreateNewMemberCommand to CreateNewPersonCommand. CreateNewPersonCommand extended implementation.

--- Skeleton/src/com/project/oop/task/management/commands/creation/CreateNewPersonCommand.java
+++ Skeleton/src/com/project/oop/task/management/commands/creation/CreateNewPersonCommand.java
@@ -3,32 +3,45 @@ package com.project.oop.task.management.commands.creation;
 import com.project.oop.task.management.commands.contracts.Command;
 import com.project.oop.task.management.core.TaskManagementRepositoryImpl;
 import com.project.oop.task.management.core.contracts.TaskManagementRepository;
-import com.project.oop.task.management.models.MemberImpl;
+import com.project.oop.task.management.models.TaskImpl;
 import com.project.oop.task.management.models.contracts.Member;
 import com.project.oop.task.management.utils.ValidationHelper;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class CreateNewMemberCommand implements Command {
+public class CreateNewPersonCommand implements Command {
     public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
 
     private String name;
 
     private final TaskManagementRepository repository;
-    public CreateNewMemberCommand(TaskManagementRepositoryImpl taskManagementRepository) {
+    public CreateNewPersonCommand(TaskManagementRepositoryImpl taskManagementRepository) {
         this.repository = taskManagementRepository;
     }
 
     @Override
     public String execute( List<String> parameters) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter person name:");
-        String name = scanner.nextLine();
-        parameters.add(name);
 
-        ValidationHelper.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+        System.out.println("Please enter person name:");
+        String personName = "";
+        boolean nameIsValid = false;
+        while (!nameIsValid) {
+            personName = scanner.nextLine();
+            try {
+                TaskImpl.validateTitle(personName);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                personName = "";
+            }
+            if (!personName.equals("")) {
+                nameIsValid = true;
+                parameters.add(personName);
+            }
+        }
+
+        name = parameters.get(0);
         if (repository.getMembers().stream().anyMatch(member -> member.getName().equals(name))){
             throw new IllegalArgumentException("A person with the same name already exists.");
         }

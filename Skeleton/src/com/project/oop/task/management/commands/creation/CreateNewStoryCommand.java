7c89ccecde2b0605470701b4d59d6c0e31653ba8
Nikol Stoyneva <nikol.stoyneva@abv.bg>
2023-05-03 16:19:00 +0300

    Change CreateNewStoryCommand messages

--- Skeleton/src/com/project/oop/task/management/commands/creation/CreateNewStoryCommand.java
+++ Skeleton/src/com/project/oop/task/management/commands/creation/CreateNewStoryCommand.java
@@ -17,12 +17,16 @@ public class CreateNewStoryCommand implements Command{
     public static final String STORY_CREATED =
             "Story with id: %d and title: %s was created.";
     public static final String NOT_A_MEMBER_MESSAGE =
-            "You are not part of the team and cannot create a new story! Please enter a valid assignee:";
+            "You are not part of the team and cannot create a new story! " +
+                    "Please enter a valid assignee or 'cancel' if you want to exit: ";
     public static final String BOARD_IS_NOT_FOUNDED =
-            "This board is not founded in your team! Please enter a valid board name:";
+            "This board is not founded in your team! " +
+                    "Please enter a valid board name or 'cancel' if you want to exit: ";
     public static final String TEAM_IS_NOT_FOUNDED =
-            "There is no team with this name. Please enter a valid team name:";
-    public static final String INVALID_INPUT = "Invalid input! Enter a new command, please:";
+            "There is no team with this name. " +
+                    "Please enter a valid team name or 'cancel' if you want to exit: ";
+    public static final String INVALID_INPUT = "Invalid input! " +
+            "Enter a new command:";
 
 
     public static int EXPECTED_NUMBER_OF_ARGUMENTS = 7;
@@ -57,8 +61,7 @@ public class CreateNewStoryCommand implements Command{
             }
         }
 
-        System.out.println("Please enter in which board you would like to add this story or " +
-                "'cancel' if you want to exit: : ");
+        System.out.println("Please enter in which board you would like to add this story: ");
         boolean boardIsValid = false;
         while (!boardIsValid) {
             targetBoard = scanner.nextLine();
@@ -77,7 +80,7 @@ public class CreateNewStoryCommand implements Command{
             }
         }
 
-        System.out.println("Please enter your name, as assignee or 'cancel' if you want to exit: ");
+        System.out.println("Please enter your name, as assignee:");
         boolean assigneeIsValid = false;
         while (!assigneeIsValid) {
             assignee = scanner.nextLine();
@@ -92,7 +95,7 @@ public class CreateNewStoryCommand implements Command{
             }
         }
 
-        System.out.println("Please enter a valid title or 'cancel' if you want to exit: ");
+        System.out.println("Please enter a valid title:");
         boolean isValidTitle = false;
         while (!isValidTitle) {
             title = scanner.nextLine();
@@ -112,7 +115,7 @@ public class CreateNewStoryCommand implements Command{
         }
 
 
-        System.out.println("Please enter a valid description or 'cancel' if you want to exit: ");
+        System.out.println("Please enter a valid description:");
         boolean isValidDescription = false;
         while (!isValidDescription) {
             description = scanner.nextLine();
@@ -131,7 +134,7 @@ public class CreateNewStoryCommand implements Command{
             }
         }
 
-        System.out.println("Please enter a valid priority or 'cancel' if you want to exit: ");
+        System.out.println("Please enter a valid priority:");
         boolean isValidPriority = false;
         while (!isValidPriority) {
             String input = scanner.nextLine();
@@ -149,7 +152,7 @@ public class CreateNewStoryCommand implements Command{
             }
         }
 
-        System.out.println("Please enter a valid size or 'cancel' if you want to exit: ");
+        System.out.println("Please enter a valid size:");
         boolean isValidSize = false;
         while (!isValidSize) {
             String input = scanner.nextLine();

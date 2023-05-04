39d5504654346d060bacf09fe467dff262a2fd3b
Tatyana Velkova <tatyana.velkova@gmail.com>
2023-05-04 13:58:09 +0300

    FilterBugsByAssigneeCommand

--- Skeleton/src/com/project/oop/task/management/commands/listing/FilterBugsByAssigneeCommand.java
+++ Skeleton/src/com/project/oop/task/management/commands/listing/FilterBugsByAssigneeCommand.java
@@ -2,19 +2,54 @@ package com.project.oop.task.management.commands.listing;
 
 import com.project.oop.task.management.commands.contracts.Command;
 import com.project.oop.task.management.core.TaskManagementRepositoryImpl;
+import com.project.oop.task.management.models.contracts.Bug;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class FilterBugsByAssigneeCommand implements Command {
-    public static int EXPECTED_NUMBER_OF_ARGUMENTS;
+
+    private List<Bug> filteredBugs = new ArrayList<>();
+    private String assigneeName;
+
 
     private final TaskManagementRepositoryImpl repository;
     public FilterBugsByAssigneeCommand(TaskManagementRepositoryImpl taskManagementRepository) {
-        this.repository = new TaskManagementRepositoryImpl();
+        this.repository = taskManagementRepository;
     }
 
     @Override
     public String execute(List<String> parameters) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter assignee name:");
+        boolean nameIsValid = false;
+
+        while (!nameIsValid) {
+            assigneeName = scanner.nextLine();
+            if (assigneeName.equals("cancel")) {
+                throw new IllegalArgumentException("Command is terminated. Please enter a new command:");
+            }
+            try {
+                if (!repository.getMembers().stream().anyMatch(member -> member.getName().equals(assigneeName))) {
+                    throw new IllegalArgumentException("No assignee with this name");
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                assigneeName = "";
+            }
+
+            if (!assigneeName.equals("")) {
+                filteredBugs = repository.getBugs().stream().filter(bug -> bug.getAssignee().equals(assigneeName)).collect(Collectors.toList());
+                if (filteredBugs.size() == 0) {
+                    return String.format("No bugs assigned to this person.");
+                }
+            }
+        }
+
         return null;
     }
+
 }
--- Skeleton/src/com/project/oop/task/management/utils/ListingHelpers.java
+++ Skeleton/src/com/project/oop/task/management/utils/ListingHelpers.java
@@ -15,4 +15,5 @@ public class ListingHelpers {
         return String.join(JOIN_DELIMITER + System.lineSeparator(), result).trim();
     }
 
+
 }

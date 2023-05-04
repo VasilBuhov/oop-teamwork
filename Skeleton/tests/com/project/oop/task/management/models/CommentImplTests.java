8f809a22a43382f776097ccd2b5bc3705ce057b1
Nikol Stoyneva <nikol.stoyneva@abv.bg>
2023-05-04 23:12:45 +0300

    Add CommentImplTests

--- Skeleton/tests/com/project/oop/task/management/models/CommentImplTests.java
+++ Skeleton/tests/com/project/oop/task/management/models/CommentImplTests.java
@@ -1,4 +1,48 @@
 package com.project.oop.task.management.models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CommentImplTests {
+
+    @Test
+    public void construct_Should_CreateCategory_When_NameIsValid() {
+        CommentImpl comment = initializeTestComment();
+        Assertions.assertNotNull(comment);
+    }
+
+    @Test
+    public void constructor_Should_InitializeContent_When_ArgumentsAreValid() {
+        CommentImpl comment = initializeTestComment();
+        Assertions.assertNotNull(comment.getContent());
+    }
+
+    @Test
+    public void constructor_Should_InitializeAuthor_When_ArgumentsAreValid() {
+        CommentImpl comment = initializeTestComment();
+        Assertions.assertNotNull(comment.getAuthor());
+    }
+
+    @Test
+    public void toString_Should_ReturnResultFromViewInfo() {
+        //Arrange
+        CommentImpl comment = initializeTestComment();
+
+        //Act
+        String string = String.format("***********%n") +
+                String.format("Comment: " + comment.getContent() + "%n") +
+                String.format("Author: %s%n", comment.getAuthor()) +
+                String.format("***********%n");
+
+        //Assert
+        assertEquals(string, comment.toString());
+    }
+
+    public static CommentImpl initializeTestComment() {
+        return new CommentImpl(
+                "This is test.",
+                "Vasil");
+    }
 }

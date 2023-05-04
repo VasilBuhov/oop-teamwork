bb53f09d8064cb9bfa1478a54a1b5fad24453911
Nikol Stoyneva <nikol.stoyneva@abv.bg>
2023-05-04 23:17:24 +0300

    Add EventLogImplTests

---
+++ Skeleton/tests/com/project/oop/task/management/models/EventLogImplTests.java
@@ -0,0 +1,25 @@
+package com.project.oop.task.management.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class EventLogImplTests {
+
+    @Test
+    public void should_ThrowException_When_ArgumentsAreNotValid() {
+        // Arrange, Act, Assert
+        assertThrows(IllegalArgumentException.class, () ->
+                new EventLogImpl(""));
+    }
+
+    @Test
+    public void getDescription_Should_ReturnDescription_When_EventLogIsCreated() {
+        // Arrange
+        EventLogImpl eventLog = new EventLogImpl("test");
+
+        // Act, Assert
+        assertEquals("test", eventLog.getDescription());
+    }
+}

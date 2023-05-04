184dcd12b12e7c55d0e50dd56ec77e0bdaa85832
Tatyana Velkova <tatyana.velkova@gmail.com>
2023-05-04 18:54:23 +0300

    FilterFeedbackByStatusCommand implemented; changes in ParsingHelper

--- Skeleton/src/com/project/oop/task/management/utils/ListingHelpers.java
+++ Skeleton/src/com/project/oop/task/management/utils/ListingHelpers.java
@@ -1,16 +1,32 @@
 package com.project.oop.task.management.utils;
 
-import com.project.oop.task.management.models.contracts.Printable;
+import com.project.oop.task.management.models.contracts.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListingHelpers {
     public static final String JOIN_DELIMITER = "*********************";
+
+    public static String bugsToString(List<Bug> bugs){
+        return elementsToString(bugs);
+    }
+
+    public static String storiesToString(List<Story> stories){
+        return elementsToString(stories);
+    }
+
+    public static String feedbacksToString(List<Feedback> feedbacks){
+        return elementsToString(feedbacks);
+    }
+
+    public static String tasksToString(List<Task> tasks){
+        return elementsToString(tasks);
+    }
     public static <T extends Printable> String elementsToString(List<T> elements) {
         List<String> result = new ArrayList<>();
         for (T element : elements) {
-            result.add(element.toString());
+            result.add(element.getAsString());
         }
         return String.join(JOIN_DELIMITER + System.lineSeparator(), result).trim();
     }

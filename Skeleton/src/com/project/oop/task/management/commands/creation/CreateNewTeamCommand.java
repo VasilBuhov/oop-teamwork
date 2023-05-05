7f063b977f294acd7a5153c12cda6c8f0cffafdc
Nikol Stoyneva <nikol.stoyneva@abv.bg>
2023-05-05 15:47:41 +0300

    Add CreateNewStoryCommandTests

--- Skeleton/src/com/project/oop/task/management/commands/creation/CreateNewTeamCommand.java
+++ Skeleton/src/com/project/oop/task/management/commands/creation/CreateNewTeamCommand.java
@@ -37,31 +37,27 @@ public class CreateNewTeamCommand implements Command {
         boolean teamIsValid = false;
         while (!teamIsValid) {
             name = scanner.nextLine();
-            teamIsValid = isTeamValid(parameters, teamIsValid);
+            if (repository.getTeams().stream().noneMatch(team1 -> team1.getName().equals(name))) {
+                repository.isItCancel(name, INVALID_INPUT);
+                try {
+                    TeamImpl.validateName(name);
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage() + " Try again or enter 'cancel' to exit:");
+                    name = "";
+                }
+                if (!name.equals("")) {
+                    teamIsValid = true;
+                    parameters.add(name);
+                }
+            } else {
+                System.out.println(TEAM_ALREADY_EXIST);
+            }
         }
 
+        ValidationHelper.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
 
+        Team team1 = repository.createNewTeam(name);
         return String.format(TEAM_CREATED, name);
     }
-
-    private boolean isTeamValid(List<String> parameters, boolean teamIsValid) {
-        if (repository.getTeams().stream().noneMatch(team1 -> team1.getName().equals(name))) {
-            repository.isItCancel(name, INVALID_INPUT);
-            try {
-                TeamImpl.validateName(name);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage() + " Try again or enter 'cancel' to exit:");
-                name = "";
-            }
-            if (!name.equals("")) {
-                teamIsValid = true;
-                parameters.add(name);
-                ValidationHelper.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
-                Team team1 = repository.createNewTeam(name);
-            }
-        } else {
-            System.out.println(TEAM_ALREADY_EXIST);
-        }
-        return teamIsValid;
-    }
 }
+
--- Skeleton/tests/com/project/oop/task/management/commands/CreateNewTeamCommandTests.java
+++ Skeleton/tests/com/project/oop/task/management/commands/CreateNewTeamCommandTests.java
@@ -5,58 +5,71 @@ import com.project.oop.task.management.commands.creation.CreateNewTeamCommand;
 import com.project.oop.task.management.core.TaskManagementRepositoryImpl;
 import com.project.oop.task.management.models.TeamImpl;
 import com.project.oop.task.management.models.contracts.Team;
-import com.project.oop.task.management.utils.ValidationHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 public class CreateNewTeamCommandTests {
-
-    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
-    public static final String ENTER_TEAM_NAME_MESSAGE =
-            "Please enter the name of your new team or 'cancel' to exit:";
-    public static final String TEAM_ALREADY_EXIST =
-            "Team with this name already exist. Please enter a valid team name or 'cancel' if you want to exit:";
-    public static final String TEAM_CREATED =
-            "Team with name %s was created!";
-    public static final String INVALID_INPUT =
-            "Command is terminated. Please enter a new command:";
-    Command createNewTeamCommand;
-    TaskManagementRepositoryImpl repository;
+    private Command command;
+    private TaskManagementRepositoryImpl repository;
 
     @BeforeEach
-    public void beforeEach() {
-        repository = new TaskManagementRepositoryImpl();
-        createNewTeamCommand= new CreateNewTeamCommand(repository);
+    public void before(){
+        this.repository = new TaskManagementRepositoryImpl();
+        this.command = new CreateNewTeamCommand(repository);
     }
 
     @Test
-    public void should_ThrowException_When_ArgumentCountDifferentThanExpected() {
-        String name = "valid";
-        String teamIsValid = "false";
-        List<String> parameters = new ArrayList<>();
-        if (repository.getTeams().stream().noneMatch(team1 -> team1.getName().equals("valid"))) {
-            repository.isItCancel(name, INVALID_INPUT);
-            try {
-                TeamImpl.validateName(name);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage() + " Try again or enter 'cancel' to exit:");
-                name = "";
-            }
-            if (!name.equals("")) {
-                teamIsValid = "true";
-                parameters.add(name);
-                ValidationHelper.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
-                Team team1 = repository.createNewTeam(name);
-            }
-        } else {
-            System.out.println(TEAM_ALREADY_EXIST);
-        }
-        Assertions.assertEquals("true", teamIsValid);
+    public void execute_Should_ThrowException_When_NameLengthNotValid(){
+        List<String> params = new ArrayList<>();
+        String name = "Test";
+
+        InputStream in = new ByteArrayInputStream(name.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> command.execute(params));
+    }
+
+    @Test
+    public void execute_Should_ThrowException_When_InputIsEqualToCancel(){
+        List<String> params = new ArrayList<>();
+        String name = "cancel";
+
+        InputStream in = new ByteArrayInputStream(name.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.execute(params));
+    }
+
+    @Test
+    public void execute_Should_ThrowException_When_NameAlreadyExists(){
+        Team team = new TeamImpl("IntelliNinjas");
+        repository.createNewTeam(team.getName());
+        List<String> params = new ArrayList<>();
+        params.add(team.getName());
+        String name = "IntelliNinjas";
+        InputStream in = new ByteArrayInputStream(name.getBytes());
+        System.setIn(in);
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> command.execute(params));
+    }
+
+    @Test
+    public void execute_Should_CreatePerson_When_NameIsValid(){
+        List<String> params = new ArrayList<>();
+        String teamName = "IntelliNinjas";
+        InputStream in = new ByteArrayInputStream(teamName.getBytes());
+        System.setIn(in);
+        command.execute(params);
+
+        Assertions.assertEquals(1, repository.getTeams().size());
     }
 
 }

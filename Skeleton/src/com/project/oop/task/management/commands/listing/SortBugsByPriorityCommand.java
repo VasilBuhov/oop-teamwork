6c329020fb6cb584d40ea210a0b0ff22744ed52f
VasilBuhov1 <kp.buhov@gmail.com>
2023-05-05 19:45:16 +0300

    Sort Bugs by severity and priority

--- Skeleton/src/com/project/oop/task/management/commands/listing/SortBugsByPriorityCommand.java
+++ Skeleton/src/com/project/oop/task/management/commands/listing/SortBugsByPriorityCommand.java
@@ -2,19 +2,41 @@ package com.project.oop.task.management.commands.listing;
 
 import com.project.oop.task.management.commands.contracts.Command;
 import com.project.oop.task.management.core.TaskManagementRepositoryImpl;
+import com.project.oop.task.management.models.BugImpl;
+import com.project.oop.task.management.models.contracts.Bug;
+import com.project.oop.task.management.models.contracts.Story;
+import com.project.oop.task.management.models.contracts.Task;
+import com.project.oop.task.management.models.enums.Priority;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortBugsByPriorityCommand implements Command {
     public static int EXPECTED_NUMBER_OF_ARGUMENTS;
 
     private final TaskManagementRepositoryImpl repository;
+
     public SortBugsByPriorityCommand(TaskManagementRepositoryImpl taskManagementRepository) {
-        this.repository = new TaskManagementRepositoryImpl();
+        this.repository = taskManagementRepository;
     }
 
     @Override
     public String execute(List<String> parameters) {
-        return null;
+       String a = repository.getBugs().stream()
+               .filter(bug -> bug.getPriority().equals(Priority.HIGH))
+               .map(bug -> bug.getPriority() + " - " + bug.getTitle())
+               .collect(Collectors.toList()).toString();
+       String b = repository.getBugs().stream()
+                .filter(bug -> bug.getPriority().equals(Priority.MEDIUM))
+                .map(bug -> bug.getPriority() + " - " + bug.getTitle())
+                .collect(Collectors.toList()).toString();
+        String c = repository.getBugs().stream()
+                .filter(bug -> bug.getPriority().equals(Priority.LOW))
+                .map(bug -> bug.getPriority() + " - " + bug.getTitle())
+                .collect(Collectors.toList()).toString();
+        return  "Bugs with high priority: "+ a +
+                "\n"+"Bugs with medium priority: " + b +
+                "\n"+"Bugs with low priority: " + c;
     }
 }
--- Skeleton/src/com/project/oop/task/management/commands/listing/SortBugsBySeverityCommand.java
+++ Skeleton/src/com/project/oop/task/management/commands/listing/SortBugsBySeverityCommand.java
@@ -2,19 +2,39 @@ package com.project.oop.task.management.commands.listing;
 
 import com.project.oop.task.management.commands.contracts.Command;
 import com.project.oop.task.management.core.TaskManagementRepositoryImpl;
+import com.project.oop.task.management.models.contracts.Bug;
+import com.project.oop.task.management.models.enums.Priority;
+import com.project.oop.task.management.models.enums.Severity;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortBugsBySeverityCommand implements Command {
     public static int EXPECTED_NUMBER_OF_ARGUMENTS;
 
     private final TaskManagementRepositoryImpl repository;
+
     public SortBugsBySeverityCommand(TaskManagementRepositoryImpl taskManagementRepository) {
         this.repository = new TaskManagementRepositoryImpl();
     }
 
     @Override
     public String execute(List<String> parameters) {
-        return null;
+        String a = repository.getBugs().stream()
+                .filter(bug -> bug.getSeverity().equals(Severity.CRITICAL))
+                .map(bug -> bug.getSeverity() + " - " + bug.getTitle())
+                .collect(Collectors.toList()).toString();
+        String b = repository.getBugs().stream()
+                .filter(bug -> bug.getSeverity().equals(Severity.MAJOR))
+                .map(bug -> bug.getPriority() + " - " + bug.getTitle())
+                .collect(Collectors.toList()).toString();
+        String c = repository.getBugs().stream()
+                .filter(bug -> bug.getSeverity().equals(Severity.MINOR))
+                .map(bug -> bug.getSeverity() + " - " + bug.getTitle())
+                .collect(Collectors.toList()).toString();
+        return  "Bugs with critical severity: "+ a +
+                "\n"+"Bugs with major severity: " + b +
+                "\n"+"Bugs with minor severity: " + c;
     }
 }

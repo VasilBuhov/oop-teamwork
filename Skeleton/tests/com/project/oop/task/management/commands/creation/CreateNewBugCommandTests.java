54f69fac3561d916dcc7dd8442395fabde0fb845
Nikol Stoyneva <nikol.stoyneva@abv.bg>
2023-05-07 11:53:40 +0300

    Add CreateNewBoardCommandTests

--- Skeleton/tests/com/project/oop/task/management/commands/creation/CreateNewBugCommandTests.java
+++ Skeleton/tests/com/project/oop/task/management/commands/creation/CreateNewBugCommandTests.java
@@ -249,4 +249,217 @@ public class CreateNewBugCommandTests {
         //Act, Assert
         Assertions.assertThrows(NoSuchElementException.class, () -> command.execute(params));
     }
+    @Test
+    public void execute_Should_ThrowException_When_BoardEqualsCancel() {
+        //Arrange
+        List<String> params = new ArrayList<>();
+
+        InputStream in = new ByteArrayInputStream(("Valid\n").getBytes());
+        System.setIn(in);
+        createPerson.execute(params);
+
+        List<String> params1 = new ArrayList<>();
+        InputStream in1 = new ByteArrayInputStream(("Team1\n").getBytes());
+        System.setIn(in1);
+        createTeam.execute(params1);
+
+        List<String> params2 = new ArrayList<>();
+        InputStream in2 = new ByteArrayInputStream(("Team1\nBoard1\n").getBytes());
+        System.setIn(in2);
+        createBoard.execute(params2);
+
+        List<String> params3 = new ArrayList<>();
+        InputStream in3 = new ByteArrayInputStream(("Valid\nTeam1\n").getBytes());
+        System.setIn(in3);
+        addPersonToTeam.execute(params3);
+
+        InputStream in4 = new ByteArrayInputStream(("Team1\ncancel\nValidTitle\nValidDescription\nHigh\nMinor\nValid\n").getBytes());
+        System.setIn(in4);
+
+        //Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.execute(params));
+    }
+
+    @Test
+    public void execute_Should_ThrowException_When_TeamEqualsCancel() {
+        //Arrange
+        List<String> params = new ArrayList<>();
+
+        InputStream in = new ByteArrayInputStream(("Valid\n").getBytes());
+        System.setIn(in);
+        createPerson.execute(params);
+
+        List<String> params1 = new ArrayList<>();
+        InputStream in1 = new ByteArrayInputStream(("Team1\n").getBytes());
+        System.setIn(in1);
+        createTeam.execute(params1);
+
+        List<String> params2 = new ArrayList<>();
+        InputStream in2 = new ByteArrayInputStream(("Team1\nBoard1\n").getBytes());
+        System.setIn(in2);
+        createBoard.execute(params2);
+
+        List<String> params3 = new ArrayList<>();
+        InputStream in3 = new ByteArrayInputStream(("Valid\nTeam1\n").getBytes());
+        System.setIn(in3);
+        addPersonToTeam.execute(params3);
+
+        InputStream in4 = new ByteArrayInputStream(("cancel\nBoard1\nValidTitle\nValidDescription\nHigh\nMinor\nValid\n").getBytes());
+        System.setIn(in4);
+
+        //Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.execute(params));
+    }
+    @Test
+    public void execute_Should_ThrowException_When_TitleEqualsCancel() {
+        //Arrange
+        List<String> params = new ArrayList<>();
+
+        InputStream in = new ByteArrayInputStream(("Valid\n").getBytes());
+        System.setIn(in);
+        createPerson.execute(params);
+
+        List<String> params1 = new ArrayList<>();
+        InputStream in1 = new ByteArrayInputStream(("Team1\n").getBytes());
+        System.setIn(in1);
+        createTeam.execute(params1);
+
+        List<String> params2 = new ArrayList<>();
+        InputStream in2 = new ByteArrayInputStream(("Team1\nBoard1\n").getBytes());
+        System.setIn(in2);
+        createBoard.execute(params2);
+
+        List<String> params3 = new ArrayList<>();
+        InputStream in3 = new ByteArrayInputStream(("Valid\nTeam1\n").getBytes());
+        System.setIn(in3);
+        addPersonToTeam.execute(params3);
+
+        InputStream in4 = new ByteArrayInputStream(("Team1\nBoard1\ncancel\nValidDescription\nHigh\nMinor\nValid\n").getBytes());
+        System.setIn(in4);
+
+        //Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.execute(params));
+    }
+    @Test
+    public void execute_Should_ThrowException_When_DescriptionEqualsCancel() {
+        //Arrange
+        List<String> params = new ArrayList<>();
+
+        InputStream in = new ByteArrayInputStream(("Valid\n").getBytes());
+        System.setIn(in);
+        createPerson.execute(params);
+
+        List<String> params1 = new ArrayList<>();
+        InputStream in1 = new ByteArrayInputStream(("Team1\n").getBytes());
+        System.setIn(in1);
+        createTeam.execute(params1);
+
+        List<String> params2 = new ArrayList<>();
+        InputStream in2 = new ByteArrayInputStream(("Team1\nBoard1\n").getBytes());
+        System.setIn(in2);
+        createBoard.execute(params2);
+
+        List<String> params3 = new ArrayList<>();
+        InputStream in3 = new ByteArrayInputStream(("Valid\nTeam1\n").getBytes());
+        System.setIn(in3);
+        addPersonToTeam.execute(params3);
+
+        InputStream in4 = new ByteArrayInputStream(("Team1\nBoard1\nValidTitle\ncancel\nHigh\nMinor\nValid\n").getBytes());
+        System.setIn(in4);
+
+        //Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.execute(params));
+    }
+
+    @Test
+    public void execute_Should_ThrowException_When_AssigneeEqualsCancel() {
+        //Arrange
+        List<String> params = new ArrayList<>();
+
+        InputStream in = new ByteArrayInputStream(("Valid\n").getBytes());
+        System.setIn(in);
+        createPerson.execute(params);
+
+        List<String> params1 = new ArrayList<>();
+        InputStream in1 = new ByteArrayInputStream(("Team1\n").getBytes());
+        System.setIn(in1);
+        createTeam.execute(params1);
+
+        List<String> params2 = new ArrayList<>();
+        InputStream in2 = new ByteArrayInputStream(("Team1\nBoard1\n").getBytes());
+        System.setIn(in2);
+        createBoard.execute(params2);
+
+        List<String> params3 = new ArrayList<>();
+        InputStream in3 = new ByteArrayInputStream(("Valid\nTeam1\n").getBytes());
+        System.setIn(in3);
+        addPersonToTeam.execute(params3);
+
+        InputStream in4 = new ByteArrayInputStream(("Team1\nBoard1\nValidTitle\nValidDescription\nHigh\nMinor\ncancel\n").getBytes());
+        System.setIn(in4);
+
+        //Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.execute(params));
+    }
+    @Test
+    public void execute_Should_ThrowException_When_PriorityEqualsCancel() {
+        //Arrange
+        List<String> params = new ArrayList<>();
+
+        InputStream in = new ByteArrayInputStream(("Valid\n").getBytes());
+        System.setIn(in);
+        createPerson.execute(params);
+
+        List<String> params1 = new ArrayList<>();
+        InputStream in1 = new ByteArrayInputStream(("Team1\n").getBytes());
+        System.setIn(in1);
+        createTeam.execute(params1);
+
+        List<String> params2 = new ArrayList<>();
+        InputStream in2 = new ByteArrayInputStream(("Team1\nBoard1\n").getBytes());
+        System.setIn(in2);
+        createBoard.execute(params2);
+
+        List<String> params3 = new ArrayList<>();
+        InputStream in3 = new ByteArrayInputStream(("Valid\nTeam1\n").getBytes());
+        System.setIn(in3);
+        addPersonToTeam.execute(params3);
+
+        InputStream in4 = new ByteArrayInputStream(("Team1\nBoard1\nValidTitle\nValidDescription\ncancel\nMinor\nValid\n").getBytes());
+        System.setIn(in4);
+
+        //Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.execute(params));
+    }
+
+    @Test
+    public void execute_Should_ThrowException_When_SeverityEqualsCancel() {
+        //Arrange
+        List<String> params = new ArrayList<>();
+
+        InputStream in = new ByteArrayInputStream(("Valid\n").getBytes());
+        System.setIn(in);
+        createPerson.execute(params);
+
+        List<String> params1 = new ArrayList<>();
+        InputStream in1 = new ByteArrayInputStream(("Team1\n").getBytes());
+        System.setIn(in1);
+        createTeam.execute(params1);
+
+        List<String> params2 = new ArrayList<>();
+        InputStream in2 = new ByteArrayInputStream(("Team1\nBoard1\n").getBytes());
+        System.setIn(in2);
+        createBoard.execute(params2);
+
+        List<String> params3 = new ArrayList<>();
+        InputStream in3 = new ByteArrayInputStream(("Valid\nTeam1\n").getBytes());
+        System.setIn(in3);
+        addPersonToTeam.execute(params3);
+
+        InputStream in4 = new ByteArrayInputStream(("Team1\nBoard1\nValidTitle\nValidDescription\nHigh\ncancel\nValid\n").getBytes());
+        System.setIn(in4);
+
+        //Act, Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> command.execute(params));
+    }
 }
